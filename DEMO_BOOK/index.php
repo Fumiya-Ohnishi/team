49034152f3fd3f33a8d89b6c7eccffeb4c6e6d1d
oonisifumiya <soccer.14.2.25@gmail.com>
2021-07-29 11:35:32 +0900

    commit index.php style.css

---
+++ DEMO_BOOK/index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>DEMO_BOOK</title>
+</head>
+
+<body>
+    <div class="container">
+        <header>
+            <h1 class="site-title">
+                <a href="index.html"><img src="img/logo.svg" alt="PHOTO BOOK"></a>
+            </h1>
+        </header>
+
+        <main>
+            <div class="main_img">
+                <img src="img/mainvisual.jpg" alt="メイン画像">
+            </div>
+
+            <section id="index">
+                <div class="index_title">
+                    <h2>INDEX</h2>
+                </div>
+                <ol>
+                    <li>タイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトル</li>
+                    <li>タイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトル</li>
+                    <li>タイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトル</li>
+                    <li>タイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトル</li>
+                    <li>タイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトル</li>
+                </ol>
+            </section>
+
+            <section id="detail">
+                <div class="detail_title">
+                    <h2>DETAIL</h2>
+                </div>
+                <div class="detail_wrapper">
+                    <div class="detail_img">
+                        <img src="img/detail.jpg" alt="詳細画像">
+                    </div>
+                    <div class="book_wrapper">
+                        <div class="book_title">
+                            タイトルタイトルタイトル
+                        </div>
+                        <ul class="index_list">
+                            <dl>
+                                <dt>
+                                    著者
+                                </dt>
+                                <dd>
+                                    タイトルタイトルタイトル
+                                </dd>
+                                <dt>
+                                    出版社
+                                </dt>
+                                <dd>
+                                    タイトルタイトルタイトル
+                                </dd>
+                                <dt>
+                                    発行日
+                                </dt>
+                                <dd>
+                                    タイトルタイトルタイトル
+                                </dd>
+                            </dl>
+                            <p>
+                                テキストテキストテキストテキストテキストテキスト
+                                テキストテキストテキストテキストテキストテキスト
+                            </p>
+                            <a class="link" href="#">オンラインストアでみる</a>
+                    </div>
+                </div>
+            </section>
+        </main>
+
+        <footer>
+            <p class="inner">&copy; 2020 PHOTO BOOK</p>
+        </footer>
+    </div>
+</body>
+
+</html>
